00929f09c7b2486914ac2427b56c5761e43e822f
aj91854 <155680510+aj91854@users.noreply.github.com>
2026-01-05 01:35:43 +0100

    Integrate location data for Leaflet map

--- pages-layout/page_layout.php
+++ pages-layout/page_layout.php
@@ -62,6 +62,16 @@ $totalReviews = (int)$ratingData['totalReviews'];
   <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
         integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
         crossorigin=""/>
+
+  <!-- getting the locations and to js --> 
+  <script>
+  const placeData = {
+    lat: <?= $place['latitude'] ?>,
+    lng: <?= $place['longitude'] ?>,
+    address: "<?= htmlspecialchars($place['address']) ?>"
+  };
+  </script>  
+    
   <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
           integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
           crossorigin=""></script>
