9cc51c3d7cd2ef10ee0a9c295b18d0fe0ce7f612
aj91854 <155680510+aj91854@users.noreply.github.com>
2026-01-09 21:20:19 +0100

    Add Leaflet integration and enhance form layout
    Updated the create experience page to include Leaflet for map functionality and improved form structure.

--- Sites/createExp.php
+++ Sites/createExp.php
@@ -13,10 +13,18 @@ if (!isset($_SESSION['userID'])) {
 <head>
 <meta charset="UTF-8">
 <title>Create New Place - Discoverly</title>
+
+<link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+        integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
+        crossorigin=""/>
 <link rel="stylesheet" href="../css/base.css">
 <link rel="stylesheet" href="../css/createExp.css">
+
+<script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+          integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
+          crossorigin=""></script>
 <script src="../Scripts/createEXP.js"></script> 
-  <script src="../Scripts/base.js" defer></script>
+<script src="../Scripts/base.js" defer></script>
 
 </head>
 
@@ -114,6 +122,7 @@ if (!isset($_SESSION['userID'])) {
 
 </body>
 </html>
+
 
 
 
